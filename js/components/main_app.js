1be23587d95672b75426f068602412bd365197a5
adam styles <mrfawn@gmail.com>
2022-04-07 23:14:26 +1000

    added form bind

--- js/components/main_app.js
+++ js/components/main_app.js
@@ -3,7 +3,8 @@ const ogTags = Vue.createApp({
     data() {
       return {
         placeHolder: 'I am a boss app!',
-        items: [{ message: 'Foo' }, { message: 'Bar' }]
+        // items: [{ message: 'Foo' }, { message: 'Bar' }]
+        data1: 'place holder'
       }
     }
   }).mount('#app')
